f34da435101b20684bc3bab5c10f16b362549d65
long250038728 <250038728@qq.com>
2026-06-01 11:15:14 +0800

    mirg

--- mirg/merchant_clear_test.go
+++ mirg/merchant_clear_test.go
@@ -665,9 +665,6 @@ func TestMerchantClear(t *testing.T) {
 	})
 }
 
-//redis db1 set 删除
-//merchant:goods_stock:code:1249
-
 func TestMerchantRedisClear(t *testing.T) {
 	merchantId := 0
 	redis := NewRedis(1)

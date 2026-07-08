d9be49ec9a7c870b9188ac5c62f9f0edb501e1c6
long250038728 <250038728@qq.com>
2026-07-08 17:08:29 +0800

    mirg es

--- mirg/es_test.go
+++ mirg/es_test.go
@@ -15,7 +15,7 @@ func TestGoodsStockUpdates(t *testing.T) {
 	const (
 		index      = "goods_stock"
 		merchantID = 0
-		code       = "123456654321"
+		code       = "123456"
 	)
 
 	query := elastic.NewBoolQuery().Must(
@@ -46,4 +46,19 @@ func TestGoodsStockUpdates(t *testing.T) {
 		t.Fatal(err)
 	}
 	fmt.Printf("Updated docs: %d\n", updateRes.Updated)
+}
+
+func TestESUpdates(t *testing.T) {
+	ctx := context.Background()
+	client := NewEs()
+
+	updateRes, err := client.Update().Index("import_sale_report.2026").Id("20260629_sale_order_prize_577_1513_4165_994640").Doc(map[string]any{
+		//"cost_price":       745,
+		//"cost_price_total": 3740,
+	}).Do(ctx)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("Updated docs: %s\n", updateRes.Result)
 }
